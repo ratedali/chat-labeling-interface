21ff3e35056cef27595da66900fc66fa920e0057
Ali Salaheldin <rated.ali.7@gmail.com>
2021-01-15 21:40:55 +0200

    Use peewee models instead of raw SQL

--- src/app.py
+++ src/app.py
@@ -1,80 +1,122 @@
 import json
-import mysql.connector
-import json
-from flask import (
-    Flask,
-    Response,
-    request,
-    render_template,
-)
+from flask import *
+from peewee import *
 
-data_filter = {'العربيه','العربية'}
+bot_messages = {
+  'العربيه',
+  'العربية',
+  'Get Started',
+  'Unsubscribe',
+}
 
 #set up the database
-db = mysql.connector.connect(host='34.91.135.43',user='root',passwd='',database='mtn_db')
-mycursor = db.cursor(buffered=True)
+db = SqliteDatabase('labeled_data.db')
 
-#setup mtn_data
-json_file = open('mtn_data.json')
-data = json.load(json_file)
+class BaseModel(Model):
+  class Meta:
+    database = db
 
+class Annotator(BaseModel):
+  username = CharField(unique=True)
+  firstName = CharField()
+  lastName = CharField()
 
-def fetch_line(line_num, conv):
-   new_line_num = line_num
-  # print(line_num)
-   for line in conv[line_num:]:
-     new_line_num += 1
-     if 'MTN Sudan' not in line[1] and line[3] not in data_filter:
-      # print(any(data_filter) == line[3])
-#       if line[3] in data_filter:
-#           print('i am here')
-       print(line)
-       return line[3],new_line_num
-   return 'empty', 0
+class Conversation(BaseModel):
+  id = IntegerField(primary_key=True)
+  annotator = ForeignKeyField(Annotator, backref='conversations', on_delete='SET NULL')
+  completed = BooleanField(default=False)
 
-username = "test_user"
+class Message(BaseModel):
+  conversation = ForeignKeyField(Conversation, backref='messages')
+  number = IntegerField()
+  content = TextField()
+  category = CharField(null=True)
+  subcategory = CharField(null=True)
 
 app = Flask(__name__)
+db.connect()
+db.create_tables([Annotator, Conversation, Message])
 
-with open('mtn_data.json') as json_file:
-  data = json.load(json_file)
+# Test Annotator
+annotator, _created = Annotator.get_or_create(username="annotator123", defaults={"firstName": "Mukhtar", "lastName": "Yahya"})
+
+# Load data
+with open('data.json') as json_file:
+  data = json.load(json_file)[1:]
+num_conversations = len(data)
 
 @app.route('/')
 def example():
-  mycursor.execute("SELECT conv_num,line_num FROM request_manager WHERE username = %s AND ended = false",(username,))
-  query = mycursor.fetchone()
-  # if the username dont have ended = false (does not have a nonfinished conversation) take a new conversation and create its entry
-  if not query:
-     mycursor.execute("SELECT MAX(conv_num) FROM request_manager")
-     conv_num = mycursor.fetchone()[0] + 1
-     line_num = 0
-     mycursor.execute("INSERT INTO request_manager (username,conv_num,line_num,ended) VALUES (%s,%s,%s,%s)",(username,conv_num,line_num,0))
-     db.commit()
-  #if ended = true we fetch conv_num and line_num
-  else:
-     conv_num = query[0]
-     line_num = query[1]
-  #get the conversation from json file
-  conv = data[conv_num]
-  output_line,line_num = fetch_line(line_num, conv)
-  return render_template('example.html', output_line=output_line, line_num=line_num, username=username, conv_num=conv_num)
+  message = fetch_message(annotator)
+  return render_template('example.html',
+                         message=message,
+                         annotator=annotator)
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
-  conv_num = int(request.form['conversationNumber'])
-  output_line = request.form['message']
-  line_num = int(request.form['lineNum'])
-  category = request.form['category']
+  conv_id = int(request.form['conv_id'])
+  conversation = Conversation.get(id=conv_id)
+
+  message_num = int(request.form['message_num'])
+  content = request.form['message']
+  message = Message(conversation=conversation,
+                           number=message_num,
+                           content=content)
+
+  message.category = request.form['category']
   if 'subcategory' in request.form:
-    subcategory = request.form['subcategory']
-
-  mycursor.execute("INSERT INTO data (conv_num,agent,text, label) VALUES (%s,%s,%s,%s)",(conv_num,username,output_line,category))
-  mycursor.execute("UPDATE request_manager SET line_num = %s WHERE conv_num = %s",(line_num,conv_num,))
-  db.commit()
-
-  conv = data[conv_num]
-  if line_num >= len(conv):
-     mycursor.execute("UPDATE request_manager SET ended = %s WHERE conv_num = %s",(1,conv_num,))
-     db.commit()
+    message.subcategory = request.form['subcategory']
+  message.save()
 
   return Response(status=200)
+
+
+def fetch_message(annotator):
+  """Fetch the next message the annotator needs to label"""
+  conversation = fetch_conversation(annotator)
+  if conversation is None:
+    return None
+  previous_num = (Message
+                  .select(fn.Max(Message.number))
+                  .where(Message.conversation == conversation)
+                  .scalar())
+  start = previous_num + 1 if previous_num is not None else 0
+  conv_data = data[conversation.id]
+  for num, message in enumerate(conv_data[start:]):
+    sender = message[1]
+    content = message[3]
+    if (sender != 'MTN Sudan'
+        and content not in bot_messages):
+      return Message(conversation=conversation,
+                     number=start+num,
+                     content=content)
+  conversation.completed = True
+  conversation.save()
+  return fetch_message(annotator)
+
+
+def fetch_conversation(annotator):
+  """Fetch the current conversation for an annotator"""
+  # see if the annotator has incomplete work
+  query = (Conversation
+           .select()
+           .where((Conversation.annotator == annotator) & (Conversation.completed == False))
+           .order_by(Conversation.id))
+  # in that case, get the first unfinished conversation
+  for conversation in query:
+    return conversation
+  # otherwise fetch a new conversation
+  # and add it to the database
+  conv_id = 0
+  prev_conv_id = (Conversation
+                  .select(fn.Max(Conversation.id))
+                  .scalar())
+  if prev_conv_id is not None:
+    conv_id = prev_conv_id + 1
+  if conv_id < num_conversations:
+    return Conversation.create(id=conv_id,
+                               annotator=annotator,
+                               completed=False)
+  return None
+
